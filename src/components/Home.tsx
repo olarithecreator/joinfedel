516116086f79188843569b3a4faddbbdfd3b354e
olarithecreator <leaders_fedel@outlook.com>
2025-12-24 08:15:06 +0100

    Remove duplicate Home component definition

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -19,24 +19,3 @@ const Home = () => {
 };
 
 export default Home;
-import Hero from './Hero';
-import About from './About';
-import KeyFocus from './KeyFocus';
-import LeadershipJourney from './LeadershipJourney';
-import Impact from './Impact';
-import Contact from './Contact';
-
-const Home = () => {
-  return (
-    <>
-      <Hero />
-      <About />
-      <KeyFocus />
-      <LeadershipJourney />
-      <Impact />
-      <Contact />
-    </>
-  );
-};
-
-export default Home;
